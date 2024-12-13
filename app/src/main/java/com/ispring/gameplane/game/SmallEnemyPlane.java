1b24f3c799e0476a91fb1685c90b1e5add9cef67
Tienchan27 <trandinhtien05@gmail.com>
2024-12-13 17:43:59 +0700

    Update SmallEnemyPlane.java

--- app/src/main/java/com/ispring/gameplane/game/SmallEnemyPlane.java
+++ app/src/main/java/com/ispring/gameplane/game/SmallEnemyPlane.java
@@ -6,7 +6,7 @@ public class SmallEnemyPlane extends EnemyPlane {
 
     public SmallEnemyPlane(Bitmap bitmap){
         super(bitmap);
-        setPower(1);//1 health (1 bullets to kill)
+        setPower(2);//2 health (2 bullets to kill)
         setValue(1000);//Give 1000 points when destroyed
     }
     //
